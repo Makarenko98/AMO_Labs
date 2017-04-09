1263ef3d3177e182460cc2de5198acaa9e615ce4
Makarenko <demo6112@gmail.com>
2017-04-09 22:44:49 +0300

    norm func charts

--- AMO_Lab3/FuncChartWindow.xaml.cs
+++ AMO_Lab3/FuncChartWindow.xaml.cs
@@ -24,7 +24,8 @@ namespace AMO_Lab3
         public double A { get; set; } = 0;
         public double B { get; set; } = 2;
         public SeriesCollection FuncSeriesCollection { get; set; }
-        public SeriesCollection InterpolatedFuncSeriesCollection { get; set; }
+        LineSeries FuncLS;
+        LineSeries InterpolatedFuncLS;
         public Func<double, string> YFormatter { get; set; }
         public string[] Labels { get; set; }
 
@@ -57,18 +58,23 @@ namespace AMO_Lab3
                 Y[i] = MyAlgorithm.Func(X[i]);
                 interpolatedY[i] = MyAlgorithm.Lagrange(X[i], arrX, arrY);
             }
+
+            FuncLS = new LineSeries
+            {
+                Title = "Функція",
+                Values = new ChartValues<double>(Y),
+                StrokeThickness = 5
+            };
+            InterpolatedFuncLS = new LineSeries
+            {
+                Title = "Інтерпольована функція",
+                Values = new ChartValues<double>(interpolatedY)
+            };
+
             FuncSeriesCollection = new SeriesCollection
             {
-                new LineSeries
-                {
-                    Title = "Функція",
-                    Values = new ChartValues<double>(Y),
-                },
-                new LineSeries
-                {
-                    Title = "Інтерпольована функція",
-                    Values = new ChartValues<double>(interpolatedY)
-                }
+                FuncLS,
+                InterpolatedFuncLS
             };
 
             Labels = new string[n];
@@ -79,12 +85,35 @@ namespace AMO_Lab3
             }
             YFormatter = x => Math.Round(x, 3).ToString();
 
-
+            ChartFuncCB.IsChecked = true;
+            ChartInterpolatedFuncCB.IsChecked = true;
             DataContext = this;
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             ShowCharts();
+        }
+
+        private void ChartFuncCB_Checked(object sender, RoutedEventArgs e)
+        {
+            FuncLS.Visibility = Visibility.Visible;
+        }
+
+        private void ChartFuncCB_Unchecked(object sender, RoutedEventArgs e)
+        {
+            FuncLS.Visibility = Visibility.Hidden;
+        }
+
+        private void ChartInterpolatedFuncCB_Checked(object sender, RoutedEventArgs e)
+        {
+            FuncLS.StrokeThickness = 5;
+            InterpolatedFuncLS.Visibility = Visibility.Visible;
+        }
+
+        private void ChartInterpolatedFuncCB_Unchecked(object sender, RoutedEventArgs e)
+        {
+            FuncLS.StrokeThickness = 2;
+            InterpolatedFuncLS.Visibility = Visibility.Hidden;
         }
     }
 }
