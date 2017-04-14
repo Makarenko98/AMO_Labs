d12427130aee12ce96d4618bff67344219892e6a
Makarenko <demo6112@gmail.com>
2017-04-14 19:09:58 +0300

    90%

--- AMO_Lab4/MainWindow.xaml.cs
+++ AMO_Lab4/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace AMO_Lab4
 {
@@ -20,9 +10,62 @@ namespace AMO_Lab4
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SeriesCollection FuncSeriesCollection { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+        public string[] Labels { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            aTB.Text = "0";
+            bTB.Text = "8";
+            eTB.Text = "0,00001";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double a = 0, b = 6, eps = 0.0001;
+            if (!Double.TryParse(aTB.Text, out a) || !Double.TryParse(bTB.Text, out b) || !Double.TryParse(eTB.Text, out eps) || a >= b)
+            {
+                MessageBox.Show("Некоректні дані");
+                return;
+            }
+            resTB.Text =  MyAlgorithm.Newton(MyAlgorithm.Func, MyAlgorithm.dFunc, MyAlgorithm.ddFunc, a, b, eps).ToString().Substring(0, eTB.Text.Length);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowCharts();
+        }
+
+        private void ShowCharts()
+        {
+            int n = 50;
+            double a = -10,
+                b = 15,
+                h = (Math.Abs(a) + Math.Abs(b)) / n;
+            double[] X = new double[n]
+                , Y = new double[n];
+            X[0] = a;
+            Y[0] = MyAlgorithm.Func(a);
+            for (int i = 1; i < n; i++)
+            {
+                X[i] = X[i - 1] + h;
+                Y[i] = MyAlgorithm.Func(X[i]);
+            }
+            FuncSeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Функція",
+                    Values = new ChartValues<double>(Y)
+                }
+            };
+            YFormatter = (x) => Math.Round(x, 4).ToString();
+            Labels = new string[n];
+            for (int i = 0; i < n; i++)
+                Labels[i] = Math.Round(X[i], 3).ToString();
+            DataContext = this;
         }
     }
 }
